0c189a89334c5aea0273f09f21de3fcc9526eeb9
Rémy <remdub@live.fr>
2017-04-25 14:51:20 +0200

    Initial commit

---
+++ wp-content/themes/cv_template/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cv_temlpate
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cv_template' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cv_template' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cv_template' ), 'cv_template', '<a href="https://automattic.com/" rel="designer">Rémy Dubel</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
